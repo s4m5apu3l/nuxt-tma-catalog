7008c33132910e4351bfc5007b29f6d94653c990
slava <asperatuz@yandex.ru>
2025-01-02 17:51:59 +0700

    feat: check nuxt ui

---
+++ components/ModalContent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const modal = useModal();
+</script>
+
+<template>
+	<UModal>
+		<template #footer>
+			<UButton color="neutral" label="Close" @click="modal.close()" />
+		</template>
+	</UModal>
+</template>
+
+<style lang="css" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
+import { LazyModalContent } from '#components'
+const colorMode = useColorMode();
 
+const isDark = computed({
+	get() {
+		return colorMode.value === 'dark';
+	},
+	set() {
+		colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+	},
+});
+
+const modal = useModal();
+const resolveModalContent = defineAsyncComponent(() => import('~/components/ModalContent.vue'));
+
+const openModal = () => {
+	modal.open(resolveModalContent, { title: 'Welcome', description: 'And you can even provide a description!' });
+};
+
+const closeModal = async () => {
+	await modal.close();
+};
+
+const updateModalTitle = () => {
+	modal.patch({ title: 'Updated Welcome' });
+};
 </script>
 
 <template>
 	<div>
-		
+		<UButton :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" color="neutral" variant="ghost" @click="isDark = !isDark" />
+
+		<UButton @click="openModal">Open Modal</UButton>
+		<UButton @click="closeModal">Close Modal</UButton>
+		<UButton @click="updateModalTitle">Update Title</UButton>
 	</div>
 </template>
 
-<style lang="css" scoped>
-
+<style scoped>
+.test {
+	color: red;
+}
 </style>

05efdae0d90becf254e14e2ec515eb65f30ceda1
slava <asperatuz@yandex.ru>
2025-01-04 20:05:09 +0700

    feat: add server items mock datas

--- app/components/product/List.vue
+++ app/components/product/List.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+interface IProps {
+	status: 'idle' | 'pending' | 'success' | 'error';
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<IProps>();
+</script>
 
 <template>
 	<section>
-		<template v-if="loading">loading</template>
-		<template v-else-if="isError">error</template>
+		<template v-if="props.status === 'pending'">loading</template>
+		<template v-else-if="props.status === 'error'">error</template>
 		<template v-else>
-			<slot />
+			<section class="grid grid-cols-2 gap-4">
+				<slot />
+			</section>
 		</template>
 	</section>
 </template>
--- app/pages/category/[slug].vue
+++ app/pages/category/[slug].vue
@@ -4,16 +4,20 @@ definePageMeta({
 		return typeof route.params.slug === 'string' && isNaN(parseInt(route.params.slug, 10));
 	},
 });
-
-// const {data: }
+const route = useRoute();
+// $fetch(`/api/products?category=${route.params.slug}`)
+const { data: products, status } = useAsyncData(`${route.params.slug}`, async () => {
+	return await $fetch(`/api/products`);
+});
 </script>
 
 <template>
 	<section class="py-4">
 		<main-categories />
 		<section class="l-wrapper !mt-4">
-			<product-list>
-				<product-card />
+			<product-list :status="status">
+				<pre>{{ products }}</pre>
+				<!-- <product-card /> -->
 			</product-list>
 		</section>
 	</section>

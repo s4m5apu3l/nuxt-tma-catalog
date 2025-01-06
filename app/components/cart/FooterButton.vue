332d5456d973ec27024c592214b0934274c38e8d
slava <asperatuz@yandex.ru>
2025-01-06 13:56:29 +0700

    feat: add state cart

--- app/components/cart/FooterButton.vue
+++ app/components/cart/FooterButton.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isContainProducts = ref(false);
+
+onMounted(() => {
+	setTimeout(() => {
+		isContainProducts.value = true;
+	}, 2000);
+});
+</script>
 
 <template>
-	<div class="fixed bottom-0 w-full py-3 border-t border-zinc-300 dark:border-zinc-700 bg-zinc-50 dark:bg-zinc-900">
-		<div class="l-wrapper">
-			<UButton block>Корзина (0)</UButton>
+	<transition
+		name="slide-fade"
+		enter-active-class="transition-all duration-500 ease-out"
+		leave-active-class="transition-all duration-300 ease-in"
+		enter-class="transform translate-y-full"
+		leave-to-class="transform translate-y-full"
+	>
+		<div
+			v-show="isContainProducts"
+			class="fixed bottom-0 w-full py-3 border-t border-zinc-300 dark:border-zinc-700 bg-zinc-50 dark:bg-zinc-900"
+		>
+			<div class="l-wrapper">
+				<UButton block>Корзина (0)</UButton>
+			</div>
 		</div>
-	</div>
+	</transition>
 </template>

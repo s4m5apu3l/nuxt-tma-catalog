6558fe21a5a0f9e1dabeb2d44dad23e426126899
ssslava <aslavadoma@tuta.io>
2026-01-19 10:40:52 +0700

    init: eslint and fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+	modules: ['@nuxt/eslint', '@nuxt/ui'],
 
-  devtools: {
-    enabled: true
-  },
+	devtools: {
+		enabled: true
+	},
 
-  css: ['~/assets/css/main.css'],
+	css: ['~/assets/css/main.css'],
 
-  routeRules: {
-    '/': { prerender: true }
-  },
+	routeRules: {
+		'/': { prerender: true }
+	},
 
-  compatibilityDate: '2025-01-15',
+	compatibilityDate: '2025-01-15',
 
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
+	eslint: {
+		config: {
+			stylistic: {
+				commaDangle: 'never',
+				braceStyle: '1tbs'
+			}
+		}
+	}
 })

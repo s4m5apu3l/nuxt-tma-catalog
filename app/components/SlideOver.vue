631b774f398c590dba8d3984402bd9a304b21c3b
ssslava <aslavadoma@tuta.io>
2026-01-28 08:47:56 +0700

    feat: clear for static deploy

--- app/components/SlideOver.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-	<USlideover class="max-w-[320px]" title="Slideover with close button" close-icon="i-akar-icons:cross">
-		<template #body>
-			<div class="">contnetn page</div>
-		</template>
-	</USlideover>
-</template>
